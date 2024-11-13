366c7922f092266d595ed1a853930954da5fa73d
Anokha <[Danokhat@gmail.com>
2024-11-13 12:59:12 +0530

    code for printing D

---
+++ D.py
@@ -0,0 +1,14 @@
+# pattern for letter D
+
+for row in range(num):
+    for col in range(num//3):
+        if row in [0,1,7,8] and col in[0,1,2]:
+            print("X",end =" ")
+        else:
+            print(" ",end =" ")
+    for col in range(num):
+        if row in [2,3,4,5,6]  and col in [2,3,4,5,6]:
+            print(" ",end=" ")
+        else:
+            print("X",end=" ")
+    print()
