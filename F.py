10d90fae580f2b3a5e594b07794ce8a862c303a8
Anokha <[Danokhat@gmail.com>
2024-11-13 13:08:39 +0530

    code for letter F

---
+++ F.py
@@ -0,0 +1,9 @@
+# code for letter F
+
+for col in range(10):
+    for row in range(7):
+        if col in[2,3,6,7,8,9] and row in [2,3,4,5,6]:
+            print(" ",end=" ")
+        else:
+            print("X",end=" ")
+    print()
